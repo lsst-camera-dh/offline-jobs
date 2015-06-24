4228a0e744f66d5a074460713e44b0e5f9da6b9d
Jim Chiang <jchiang@slac.stanford.edu>
2015-06-24 16:59:16 -0700

    identify e2v vendor data and set e2v_data flag so that incident illumination power can be read directly from their LIGHTPOW keyword

--- qe_offline/v0/producer_qe_offline.py
+++ qe_offline/v0/producer_qe_offline.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import os
+import pyfits
 import lsst.eotest.sensor as sensorTest
 import siteUtils
 import eotestUtils
@@ -9,6 +10,16 @@ lambda_files = siteUtils.datacatalog_glob('*_lambda_*.fits',
                                           testtype='LAMBDA',
                                           imgtype='FLAT',
                                           description='Lambda files:')
+
+#
+# Check if file orginates from e2v; if so, set e2v_data flag.
+#
+try:
+    e2v_data = pyfits.open(lambda_files[0])[0].header['ORIGIN'].find('e2v') != -1
+    print "analyzing QE data from e2v"
+except:
+    e2v_data = False
+
 # The photodiode ratio file for BNL data.
 # @todo Set this to the correct file for production runs.
 pd_ratio_file = os.path.join(os.environ['EOTEST_DIR'], 'data', 'qe',
@@ -17,4 +28,5 @@ mask_files = eotestUtils.glob_mask_files()
 gains = eotestUtils.getSensorGains(jobname='fe55_offline')
 
 task = sensorTest.QeTask()
-task.run(sensor_id, lambda_files, pd_ratio_file, mask_files, gains)
+task.run(sensor_id, lambda_files, pd_ratio_file, mask_files, gains,
+         e2v_data=e2v_data)
