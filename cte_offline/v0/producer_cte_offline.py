54e70b14e5a7cac46c1587ac2956626e43e6048d
Jim Chiang <jchiang@slac.stanford.edu>
2015-09-14 20:29:54 -0700

    changes to accommodate still non-conforming file names; analyze high and low flux datasets in cte_offline

--- cte_offline/v0/producer_cte_offline.py
+++ cte_offline/v0/producer_cte_offline.py
@@ -3,10 +3,17 @@ import lsst.eotest.sensor as sensorTest
 import siteUtils
 
 sensor_id = siteUtils.getUnitId()
-sflat_files = siteUtils.datacatalog_glob('*_sflat_500_*.fits',
-                                         testtype='SFLAT_500',
-                                         imgtype='FLAT',
-                                         description='Superflat files:')
 
+sflat_high_files = siteUtils.datacatalog_glob('*_sflat_*500_flat_H*.fits',
+                                              testtype='SFLAT_500',
+                                              imgtype='FLAT',
+                                              description='Superflat high files:')
 task = sensorTest.CteTask()
-task.run(sensor_id, sflat_files)
+task.run(sensor_id, sflat_high_files, flux_level='high')
+
+sflat_low_files = siteUtils.datacatalog_glob('*_sflat_*500_flat_L*.fits',
+                                             testtype='SFLAT_500',
+                                             imgtype='FLAT',
+                                             description='Superflat low files:')
+task = sensorTest.CteTask()
+task.run(sensor_id, sflat_low_files, flux_level='low')
