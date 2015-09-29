fe4d5271e727584b517ddf8a5183bbb018d2d8c0
Jim Chiang <jchiang@slac.stanford.edu>
2015-09-29 14:47:39 -0700

    LSSTTD-356: add CTE errors to summary.lims output and test report * add CTE errors to BNL_T03 jobs * use gains in CTE calculations so that errors can be estimated   assuming Poisson statistics

--- cte_offline/v0/producer_cte_offline.py
+++ cte_offline/v0/producer_cte_offline.py
@@ -1,19 +1,21 @@
 #!/usr/bin/env python
 import lsst.eotest.sensor as sensorTest
 import siteUtils
+import eotestUtils
 
 sensor_id = siteUtils.getUnitId()
 
-sflat_high_files = siteUtils.datacatalog_glob('*_sflat_*500_flat_H*.fits',
+gains = eotestUtils.getSensorGains(jobname='fe55_offline')
+sflat_high_files = siteUtils.datacatalog_glob('*_sflat_500_flat_H*.fits',
                                               testtype='SFLAT_500',
                                               imgtype='FLAT',
                                               description='Superflat high files:')
 task = sensorTest.CteTask()
-task.run(sensor_id, sflat_high_files, flux_level='high')
+task.run(sensor_id, sflat_high_files, flux_level='high', gains=gains)
 
-sflat_low_files = siteUtils.datacatalog_glob('*_sflat_*500_flat_L*.fits',
+sflat_low_files = siteUtils.datacatalog_glob('*_sflat_500_flat_L*.fits',
                                              testtype='SFLAT_500',
                                              imgtype='FLAT',
                                              description='Superflat low files:')
 task = sensorTest.CteTask()
-task.run(sensor_id, sflat_low_files, flux_level='low')
+task.run(sensor_id, sflat_low_files, flux_level='low', gains=gains)
