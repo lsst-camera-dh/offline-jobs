2bd2b6e3bde404dba9b8468918319b3713270530
Jim Chiang <jchiang@slac.stanford.edu>
2016-05-17 22:42:40 -0700

    use vendorDataUtils to get system noise

--- harnessed_jobs/read_noise_offline/v0/producer_read_noise_offline.py
+++ harnessed_jobs/read_noise_offline/v0/producer_read_noise_offline.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
+import sys
 import lsst.eotest.sensor as sensorTest
 import siteUtils
 import eotestUtils
+import vendorDataUtils
 
 sensor_id = siteUtils.getUnitId()
 
@@ -14,13 +16,17 @@ bias_files = siteUtils.datacatalog_glob('*_fe55_fe55_*.fits',
                                         description='Bias files (using overscan):')
 
 gains = eotestUtils.getSensorGains(jobname='fe55_offline')
-system_noise_files = siteUtils.dependency_glob('noise_*.fits', 
-                                               jobname=siteUtils.getProcessName('system_noise'))
-if not system_noise_files:
-    system_noise_files = None
+system_noise = vendorDataUtils.getSystemNoise(gains)
+if system_noise is None:
+    print
+    print "WARNING: The system noise file is not given in"
+    print "config/%s/eotest_calibrations.cfg." % siteUtils.getSiteName()
+    print "The system noise will be set to zero for all amplifiers."
+    print
+    sys.stdout.flush()
+
 mask_files = eotestUtils.glob_mask_files()
 
 task = sensorTest.ReadNoiseTask()
-task.run(sensor_id, bias_files, gains,
-         system_noise_files=system_noise_files, mask_files=mask_files,
-         use_overscan=True)
+task.run(sensor_id, bias_files, gains, system_noise=system_noise,
+         mask_files=mask_files, use_overscan=True)
