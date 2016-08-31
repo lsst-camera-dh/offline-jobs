93ef4687d254307af39c89260660097766699e0f
Jim Chiang <jchiang@slac.stanford.edu>
2016-08-31 11:19:56 -0700

    create aperture flux as a function of row/column

--- harnessed_jobs/fe55_offline/v0/producer_fe55_offline.py
+++ harnessed_jobs/fe55_offline/v0/producer_fe55_offline.py
@@ -39,6 +39,15 @@ if nf > 0:
 sensorTest.fe55_zoom(fe55_files[0], size=250, amp=1)
 plt.savefig('%(sensor_id)s_fe55_zoom.png' % locals())
 
+#
+# Perform analysis of 9-pixel statistics for Fe55 charge clusters.
+#
+pixel_stats = sensorTest.Fe55PixelStats(fe55_files, sensor_id=sensor_id)
+pixel_stats.apflux_profile()
+plt.savefig('%(sensor_id)s_fe55_apflux_serial.png' % locals())
+pixel_stats.apflux_profile(pixel_coord='y')
+plt.savefig('%(sensor_id)s_fe55_apflux_parallel.png' % locals())
+
 # Roll-off defects mask needs an input file to get the vendor
 # geometry and will be used for all analyses.
 rolloff_mask_file = '%s_rolloff_defects_mask.fits' % sensor_id
